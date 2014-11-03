38d37f2d68b1846993e1717dfd59174fd1d243b6
Zhou Xinzi <imdreamrunner@gmail.com>
2014-11-03 16:30:00 +0800

    change condition

--- yen/calibration_manager.py
+++ yen/calibration_manager.py
@@ -7,7 +7,9 @@ def can_calibrate_front(arena, robot):
     """
     cell1_x, cell1_y = robot.x + SX[robot.d] + 2*DX[robot.d], robot.y + SY[robot.d] + 2*DY[robot.d]
     cell2_x, cell2_y = robot.x - SX[robot.d] + 2*DX[robot.d], robot.y - SY[robot.d] + 2*DY[robot.d]
-    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y)
+    cell3_x, cell3_y = robot.x + 2*DX[robot.d], robot.y + 2*DY[robot.d]
+    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y) \
+            and arena.is_obstacle(cell3_x, cell3_y)
 
 
 def can_calibrate_right(arena, robot):
@@ -16,11 +18,15 @@ def can_calibrate_right(arena, robot):
     """
     cell1_x, cell1_y = robot.x + 2*SX[robot.d] + DX[robot.d], robot.y + 2*SY[robot.d] + DY[robot.d]
     cell2_x, cell2_y = robot.x + 2*SX[robot.d] - DX[robot.d], robot.y + 2*SY[robot.d] - DY[robot.d]
-    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y)
+    cell3_x, cell3_y = robot.x + 2*SX[robot.d], robot.y + 2*SY[robot.d]
+    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y) \
+            and arena.is_obstacle(cell3_x, cell3_y)
 
 
 def can_calibrate_left(arena, robot):
     cell1_x, cell1_y = robot.x - 2*SX[robot.d] + DX[robot.d], robot.y - 2*SY[robot.d] + DY[robot.d]
     cell2_x, cell2_y = robot.x - 2*SX[robot.d] - DX[robot.d], robot.y - 2*SY[robot.d] - DY[robot.d]
-    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y)
+    cell3_x, cell3_y = robot.x - 2*SX[robot.d], robot.y - 2*SY[robot.d]
+    return arena.is_obstacle(cell1_x, cell1_y) and arena.is_obstacle(cell2_x, cell2_y) \
+            and arena.is_obstacle(cell3_x, cell3_y)
 
